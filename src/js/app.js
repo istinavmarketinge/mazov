cd5e60cf2d5672d266a4d75aae6f6c715585b340
Olya Krugova <j-olga.k@yandex.ru>
2022-11-29 14:57:43 +0300

    открывающаяся менюшка и закрытие на главной блока

--- src/js/app.js
+++ src/js/app.js
@@ -28,5 +28,32 @@ $(document).ready(() => {
     window.calendar.init();
     window.upButton.init();
 
+    if (document.querySelector('.menu-list__item-more')) {
+		document.querySelector('.menu-list__item-more').addEventListener('click', (event) => {
+			// document.querySelector('table.table-tarify').classList.toggle('isOpened');
+			event.target.classList.toggle("isOpened");
+			event.target.closest(".menu-list-glavnaya").querySelector('.menu-list').classList.toggle('isOpened');
+			console.log(event);
+		});
+	}
+    if (document.querySelector('.notification-block__close')) {
+        console.log( document.querySelector('.notification-block__close'));
+        document.querySelector('.notification-block__close').addEventListener('click', (event) =>  {
+            event.target.closest('.notification-block').classList.add('close');
+            console.log(45454);
+        });
+    }
+
+    // if (document.querySelector('.comments-all-btn')) {
+	// 	document.querySelector('.comments-all-btn').addEventListener('click', (event) => {
+	// 		// document.querySelector('table.table-tarify').classList.toggle('isOpened');
+	// 		event.target.classList.toggle("isOpen");
+	// 		event.target.closest(".comments-block__in").querySelector('.comments-personal').classList.toggle('isOpen');
+	// 		console.log(event);
+	// 	});
+	// }
+
     // TODO написать в Readme.md о возможности изменять опции календаря
 })
+
+
